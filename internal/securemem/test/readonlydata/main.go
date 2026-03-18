ddc9e085b46a11b5c12ece2eb15d90e1e1d41c08
jK <33685667+jithinkunjachan@users.noreply.github.com>
2026-03-18 15:51:23 +0100

    docs(securemem): add test documentaion

---
+++ internal/securemem/test/readonlydata/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log/slog"
+
+	"github.com/openkcm/krypton/internal/securemem"
+)
+
+const SecretData = "secret"
+
+// This example demonstrates the usage of the securemem package to create a secure memory region,
+// write data to it, mark it as read-only, and attempt to modify it,
+// which results in a system termination of the process due to the read-only protection.
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Info("PANIC RECOVERED:", "err", r)
+		}
+	}()
+
+	// create a secure memory region for the secret data
+	data, err := securemem.NewData("secret", len(SecretData))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := data.Destroy()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// copy the secret data into the secure memory region
+	copy(data.Data(), []byte(SecretData))
+
+	// make the memory region read-only
+	err = data.MarkReadOnly()
+	if err != nil {
+		panic(err)
+	}
+
+	// print the secret data
+	slog.Info(string(data.Data()))
+
+	// attempt to modify the read-only memory region, which should cause a panic due to the read-only protection
+	data.Data()[0] = 's' // panic: runtime error: invalid memory address or nil pointer dereference
+}
---
+++ internal/securemem/test/readonlydata/readonly_test.go
@@ -0,0 +1,38 @@
+package main_test
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	main "github.com/openkcm/krypton/internal/securemem/test/readonlydata"
+)
+
+// This test verifies that attempting to write to a read-only memory region results
+// in a process termination (panic). It runs the main.go program, which creates a
+// secure memory region, marks it as read-only, and then tries to modify it.
+// The test checks that an error occurs and that the output contains the expected
+// secret value and error message, confirming that the read-only protection is
+// working as intended.
+func TestReadOnlyData(t *testing.T) {
+	// given
+
+	// when
+	cmd := exec.CommandContext(t.Context(), "go", "run", "main.go")
+	output, err := cmd.CombinedOutput()
+
+	// then
+	assert.Error(t, err, "Expected an error when writing to read-only memory")
+
+	// The output should contain the secret value and an error message about
+	// the unexpected fault address,
+	outputStr := string(output)
+
+	// It should not contain the "PANIC RECOVERED:" message, as the process should
+	// have been terminated due to the read-only protection.
+	assert.NotContains(t, outputStr, "PANIC RECOVERED:")
+
+	// The output should contain the secret value followed by an error message about the unexpected fault address
+	assert.Contains(t, outputStr, main.SecretData+"\nunexpected fault address")
+}
