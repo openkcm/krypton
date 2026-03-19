ef19a263f15d7675172346b77e0c5923971aaf14
jK <33685667+jithinkunjachan@users.noreply.github.com>
2026-03-19 09:46:13 +0100

    docs(securemem): improve docs

--- internal/securemem/securemem.go
+++ internal/securemem/securemem.go
@@ -22,26 +22,34 @@ func Zero(data []byte) {
 	runtime.KeepAlive(data)
 }
 
-// readonly changes the memory protection of the provided byte slice to
-// read-only, preventing any modifications to its contents.
+// readonly changes the memory protection of the provided byte slice to read-only, preventing
+// any modifications to its contents.
 func readonly(data []byte) error {
+	// The Mprotect function changes the protection of the memory region to read-only.
 	return unix.Mprotect(data, unix.PROT_READ)
 }
 
-// readwrite changes the memory protection of the provided byte slice to
-// allow both reading and writing, enabling modifications to its contents.
+// readwrite changes the memory protection of the provided byte slice to allow both reading
+// and writing, enabling modifications to its contents.
 func readwrite(data []byte) error {
+	// The Mprotect function changes the protection of the memory region to allow both reading
+	// and writing.
 	return unix.Mprotect(data, unix.PROT_READ|unix.PROT_WRITE)
 }
 
-// unalloc securely deallocates the memory associated with the provided byte slice by first zeroing its contents,
-// then unlocking it to allow swapping, and finally unmapping it from the process's address space.
-// The function ensures that the memory is securely wiped before being released, and it returns any
-// errors encountered during the unlocking and unmapping processes.
+// unalloc securely deallocates the memory associated with the provided byte slice by first
+// zeroing its contents, then unlocking it to allow swapping, and finally unmapping it from
+// the process's address space. The function ensures that the memory is securely wiped before
+// being released, and it returns any errors encountered during the unlocking and unmapping
+// processes.
 func unalloc(data []byte) error {
 	Zero(data)
 
+	// The Munlock function unlocks the memory region, allowing it to be swapped out if necessary.
 	unlockErr := unix.Munlock(data)
+
+	// The Munmap function unmaps the memory region from the process's address space, effectively
+	// deallocating it.
 	unmapErr := unix.Munmap(data)
 
 	runtime.KeepAlive(data)
--- internal/securemem/securemem_darwin.go
+++ internal/securemem/securemem_darwin.go
@@ -6,31 +6,38 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// NoDump sets the core file size limit to zero, preventing the process
-// from generating core dumps in case of a crash or other fatal error.
+// NoDump sets the core file size limit to zero, preventing the process from generating core
+// dumps in case of a crash or other fatal error.
 func NoDump() error {
+	// Setrlimit is used to set the core file size limit to zero, which prevents the process
+	// from generating core dumps.
 	return unix.Setrlimit(unix.RLIMIT_CORE, &unix.Rlimit{Cur: 0, Max: 0})
 }
 
-// alloc allocates a memory region of the specified size, locks it to prevent
-// swapping, and returns a byte slice that references this memory.
-// The memory is allocated using the mmap system call with the MAP_ANON and MAP_PRIVATE flags,
-// which means it is not backed by any file and is private to the process.
-// Darwin does not support the MADV_DONTDUMP flag, so we skip that step and directly lock the memory after allocation.
+// alloc allocates a memory region of the specified size, locks it to prevent swapping, and
+// returns a byte slice that references this memory. The memory is allocated using the mmap
+// system call with the MAP_ANON and MAP_PRIVATE flags, which means it is not backed by any
+// file and is private to the process. Darwin does not support the MADV_DONTDUMP flag, so
+// we skip that step and directly lock the memory after allocation.
 func alloc(size int) ([]byte, error) {
+	// Mmap is used to allocate a memory region of the specified size. The parameters specify
+	// that the memory should be readable and writable, and that it should be an anonymous
+	// mapping that is private to the process.
 	data, err := unix.Mmap(
 		-1,                             // no file
 		0,                              // offset
 		size,                           // length
 		unix.PROT_READ|unix.PROT_WRITE, // to have read and write permissions
-		unix.MAP_ANON|unix.MAP_PRIVATE, // anonymous mapping, not backed by any file, and private to this process
+		unix.MAP_ANON|unix.MAP_PRIVATE, // anonymous mapping and private to this process
 	)
 	if err != nil {
 		return nil, err
 	}
 
+	// Mlock is used to lock the allocated memory region, preventing it from being swapped out to disk.
 	err = unix.Mlock(data)
 	if err != nil {
+		// even if Mlock fails, we should still unmap the memory to avoid leaks
 		errUnmap := unix.Munmap(data)
 		return nil, errors.Join(err, errUnmap)
 	}
